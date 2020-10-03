37b265ad39c5f61d1fb66cb42b32664f0832270f
Chinedum Echeta <cecheta@deloitte.co.uk>
2020-10-03 18:02:59 +0100

    Added Spotify request object

---
+++ backend/src/request.js
@@ -0,0 +1,48 @@
+const querystring = require('querystring');
+const axios = require('axios');
+
+const request = {
+  expires: null,
+  token: null,
+
+  async get(url) {
+    if (!this.token || Date.now() / 1000 > this.expires) {
+      try {
+        await this.auth();
+      } catch (error) {
+        return error;
+      }
+    }
+
+    try {
+      const response = await axios.get(`https://api.spotify.com${url}`, {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  },
+
+  async auth() {
+    const data = querystring.stringify({ grant_type: 'client_credentials' });
+    const config = {
+      auth: {
+        username: process.env.CLIENT_ID,
+        password: process.env.CLIENT_SECRET,
+      },
+    };
+
+    try {
+      const res = await axios.post('https://accounts.spotify.com/api/token', data, config);
+      this.token = res.data.access_token;
+      this.expires = Math.floor(Date.now() / 1000) + 3500;
+    } catch (error) {
+      return error;
+    }
+  },
+};
+
+module.exports = request;
