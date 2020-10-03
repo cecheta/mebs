8e8b642ed86c3338ec5ce6a8b83e3b11af2aaf49
Chinedum Echeta <cecheta@deloitte.co.uk>
2020-10-03 18:02:30 +0100

    Express server init

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+app.get('*', (req, res) => {
+  res.send('Server is up');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
