74deb480ad4b5abd2c87e41a01b5766b824b7147
Chinedum Echeta <cecheta@deloitte.co.uk>
2020-10-10 16:01:00 +0100

    Changed axios instance to use interceptor

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,19 +7,16 @@ if (process.env.NODE_ENV !== 'PRODUCTION') {
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
-app.get('/api/search', (req, res) => {
-  const query = req.query;
-  const config = { params: query };
-  axios
-    .get('/v1/search', config)
-    .then((response) => res.send(response.data))
-    .catch((error) => {
-      res.status(error.response.status).send({
-        error: { message: error.message },
-      });
-    });
+app.get('/api/search', async (req, res) => {
+  const config = { params: req.query };
+  try {
+    const response = await axios.get('/v1/search', config);
+    res.send(response.data);
+  } catch (error) {
+    res.status(error.response.status).send({ error: { message: error.message } });
+  }
 });
 
 app.get('*', (req, res) => {
