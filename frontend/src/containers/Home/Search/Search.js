ee07350f37bc7c7d1b1ebc664a7e75a10702809c
Chinedum Echeta <cecheta@deloitte.co.uk>
2020-10-11 18:38:49 +0100

    Removed dropdown from home page

--- frontend/src/containers/Home/Search/Search.js
+++ frontend/src/containers/Home/Search/Search.js
@@ -4,19 +4,14 @@ import './Search.scss';
 
 const HomeSearch = (props) => {
   const [query, setQuery] = useState('');
-  const [type, setType] = useState('all');
 
   const queryChangeHandler = (e) => {
     setQuery(e.target.value);
   };
 
-  const typeChangeHandler = (e) => {
-    setType(e.target.value);
-  };
-
   const submitHandler = async (e) => {
     e.preventDefault();
-    props.history.push(`/search?q=${encodeURIComponent(query)}&type=${type}`);
+    props.history.push(`/search?q=${encodeURIComponent(query)}&type=all`);
   };
 
   return (
@@ -24,12 +19,6 @@ const HomeSearch = (props) => {
       <h1>Mebs</h1>
       <form onSubmit={submitHandler}>
         <input type="text" name="query" placeholder="Search" value={query} onChange={queryChangeHandler} />
-        <select name="cars" value={type} onChange={typeChangeHandler}>
-          <option value="all">All</option>
-          <option value="album">Albums</option>
-          <option value="artist">Artists</option>
-          <option value="track">Songs</option>
-        </select>
         <button>Search</button>
       </form>
     </div>
