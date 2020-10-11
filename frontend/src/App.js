3f3c8ccb1bf2adc925783c9f2f42b7cc8ac9d4bf
Chinedum Echeta <cecheta@deloitte.co.uk>
2020-10-11 23:20:47 +0100

    Removed tempoary home page link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home/Home';
 import ResultsPage from './containers/ResultsPage/ResultsPage';
 import './App.scss';
@@ -7,12 +7,6 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      {/* Testing */}
-      <Link to="/">
-        <div style={{ fontSize: '20px', backgroundColor: '#fff', display: 'inline' }}>Home</div>
-      </Link>
-      {/* Testing */}
-
       <Switch>
         <Route path="/search" component={ResultsPage} />
         <Route path="/" component={Home} />
