c07a9b5052b88ad8498dae708173c04f84c8c853
Chinedum Echeta <cecheta@deloitte.co.uk>
2020-11-01 21:26:04 +0000

    Added Home to navigation

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './Navigation.scss';
 
-const Navigation = () => {
+const Navigation = (props) => {
   return (
     <ul className="Navigation">
+      {props.location.pathname !== '/account' ? (
+        <li>
+          <Link to="/account">MY ACCOUNT</Link>
+        </li>
+      ) : null}
       <li>
-        <Link to="/account">MY ACCOUNT</Link>
+        <Link to="/">HOME</Link>
       </li>
     </ul>
   );
 };
 
-export default Navigation;
+export default withRouter(Navigation);
