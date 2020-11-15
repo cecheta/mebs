4fdcb4f599db9fd26298fb85094bdf4bef19b306
Chinedum Echeta <cecheta@deloitte.co.uk>
2020-11-15 23:52:57 +0000

    Backend refactor to use routers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,85 +1,17 @@
 const express = require('express');
 const axios = require('./axios-spotify');
+const searchRouter = require('./routers/search');
+const authRouter = require('./routers/auth');
 
 if (process.env.NODE_ENV !== 'PRODUCTION') {
   require('dotenv').config();
 }
+const PORT = process.env.PORT;
 
 const app = express();
 
-const PORT = process.env.PORT;
-
-app.get('/api/search', async (req, res) => {
-  const config = { params: req.query };
-  try {
-    const response = await axios.get('/v1/search', config);
-    for (const property in response.data) {
-      let complete = false;
-      if (response.data[property].items.length < 20) {
-        complete = true;
-      }
-
-      response.data[property] = {
-        items: response.data[property].items,
-        complete,
-      };
-    }
-    res.send(response.data);
-  } catch (error) {
-    res.status(error.response.status).send({ error: { message: error.message } });
-  }
-});
-
-app.get('/api/albums/:id', async (req, res) => {
-  try {
-    const response = await axios.get(`/v1/albums/${req.params.id}`);
-    res.send(response.data);
-  } catch (error) {
-    res.status(error.response.status).send({ error: { message: error.message } });
-  }
-});
-
-app.get('/api/albums', async (req, res) => {
-  try {
-    const response = await axios.get(`/v1/albums?ids=${req.query.ids}`);
-    res.send(response.data);
-  } catch (error) {
-    res.status(error.response.status).send({ error: { message: error.message } });
-  }
-});
-
-app.get('/api/artists/:id', async (req, res) => {
-  try {
-    const requests = [axios.get(`/v1/artists/${req.params.id}`), axios.get(`/v1/artists/${req.params.id}/albums`), axios.get(`/v1/artists/${req.params.id}/top-tracks?country=GB`)];
-
-    const [artist, albums, tracks] = await Promise.all(requests);
-    res.send({
-      ...artist.data,
-      albums: albums.data.items,
-      tracks: tracks.data.tracks,
-    });
-  } catch (error) {
-    res.status(error.response.status).send({ error: { message: error.message } });
-  }
-});
-
-app.get('/api/artists', async (req, res) => {
-  try {
-    const response = await axios.get(`/v1/artists?ids=${req.query.ids}`);
-    res.send(response.data);
-  } catch (error) {
-    res.status(error.response.status).send({ error: { message: error.message } });
-  }
-});
-
-app.get('/api/songs', async (req, res) => {
-  try {
-    const response = await axios.get(`/v1/tracks?ids=${req.query.ids}`);
-    res.send(response.data.tracks);
-  } catch (error) {
-    res.status(error.response.status).send({ error: { message: error.message } });
-  }
-});
+app.use(searchRouter);
+app.use(authRouter);
 
 app.get('/api/account', async (req, res) => {
   try {
