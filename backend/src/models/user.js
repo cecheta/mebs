5f4fbc1ff4d55fe59a928f546177379b8fa2d812
Chinedum Echeta <cecheta@deloitte.co.uk>
2020-11-16 00:37:31 +0000

    Added user model schema

---
+++ backend/src/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  hash: String,
+  salt: String
+});
+
+mongoose.model('User', UserSchema);
