24208386855404db43802583a735c32ecd918bc0
Chinedum Echeta <cecheta@deloitte.co.uk>
2020-12-27 20:07:35 +0000

    Animation on search suggestions

--- frontend/src/components/HomeSearch/HomeSearch.js
+++ frontend/src/components/HomeSearch/HomeSearch.js
@@ -1,11 +1,38 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 import './HomeSearch.scss';
 
-const HomeSearch = ({ suggestion }) => {
+const HomeSearch = ({ suggestions }) => {
   const [query, setQuery] = useState('');
+  const [suggestion, setSuggestion] = useState(null);
 
   const history = useHistory();
+
+  const nodeRef = React.useRef(null);
+
+  const randomSuggestion = useCallback((suggestions) => {
+    return suggestions[Math.floor(Math.random() * suggestions.length)];
+  }, []);
+
+  useEffect(() => {
+    setSuggestion(randomSuggestion(suggestions));
+  }, [suggestions, randomSuggestion]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      let newSuggestion = randomSuggestion(suggestions);
+      while (newSuggestion === suggestion) {
+        newSuggestion = randomSuggestion(suggestions);
+      }
+
+      setSuggestion(newSuggestion);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [suggestion, suggestions, randomSuggestion]);
 
   const queryChangeHandler = (e) => {
     setQuery(e.target.value);
@@ -13,15 +40,40 @@ const HomeSearch = ({ suggestion }) => {
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    history.push(`/search?q=${encodeURIComponent(query)}&type=all`);
+    if (query) {
+      history.push(`/search?q=${encodeURIComponent(query)}&type=all`);
+    }
   };
+
+  const suggestionText = suggestion ? suggestion : '...';
 
   return (
     <div className="HomeSearch">
-      <h1>Mebs</h1>
-      <form onSubmit={submitHandler}>
-        <input type="text" name="query" placeholder={`Search for ${suggestion ? suggestion : '...'}`} value={query} onChange={queryChangeHandler} />
-        <button>Search</button>
+      <h1 className="HomeSearch__Title">Mebs</h1>
+      <form className="HomeSearch__Form" onSubmit={submitHandler}>
+        <div className="HomeSearch__Search">
+          <input className="HomeSearch__Input" type="text" name="query" value={query} onChange={queryChangeHandler} />
+          {!query ? (
+            <div className="HomeSearch__Placeholder">
+              <span>Search for </span>
+              <SwitchTransition className="HomeSearch__Placeholder">
+                <CSSTransition
+                  nodeRef={nodeRef}
+                  timeout={{
+                    enter: 300,
+                    exit: 100,
+                  }}
+                  key={suggestionText}
+                >
+                  <span className="HomeSearch__Placeholder--Suggestion" ref={nodeRef}>
+                    {suggestionText}
+                  </span>
+                </CSSTransition>
+              </SwitchTransition>
+            </div>
+          ) : null}
+        </div>
+        <button className="HomeSearch__Submit">Search</button>
       </form>
     </div>
   );
--- frontend/src/routes/Home/Home.js
+++ frontend/src/routes/Home/Home.js
@@ -1,15 +1,10 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import HomeSearch from '../../components/HomeSearch';
 import Background from '../../components/Background';
 
 const Home = () => {
   const [suggestions, setSuggestions] = useState([]);
-  const [suggestion, setSuggestion] = useState(null);
-
-  const randomSuggestion = useCallback((suggestions) => {
-    return suggestions[Math.floor(Math.random() * suggestions.length)];
-  }, []);
 
   useEffect(() => {
     (async () => {
@@ -18,29 +13,13 @@ const Home = () => {
       const filteredSuggestions = [...new Set(data)].filter((el) => el.length <= 15);
 
       setSuggestions(filteredSuggestions);
-      setSuggestion(randomSuggestion(filteredSuggestions));
     })();
-  }, [randomSuggestion]);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      let newSuggestion = randomSuggestion(suggestions);
-      while (newSuggestion === suggestion) {
-        newSuggestion = randomSuggestion(suggestions);
-      }
-
-      setSuggestion(newSuggestion);
-    }, 5000);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [suggestion, suggestions, randomSuggestion]);
+  }, []);
 
   return (
     <>
       <Background />
-      <HomeSearch suggestion={suggestion} />
+      <HomeSearch suggestions={suggestions} />
     </>
   );
 };
